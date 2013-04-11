8e0a44357c95deb3d2b11078da9a259ab70961ef
leena <leena.sn@multunus.com>
2013-04-12 01:01:39 +0530

    DRY, refactored the Google Engine Read to a new class

--- jobs/radiators.rb
+++ jobs/radiators.rb
@@ -1,35 +1,51 @@
 require 'google_drive'
 
-SPREADSHEET_KEY = "0ApUPwJdQvqT_dDI0X3ZUSUFSSkFRa19oMEZUakVDZGc"
+SPREADSHEET_KEYS = ["0Am5kxIePmFmbdFBrWElZZW5jZVQwODRpRXNsMEFWRkE",
+                    "0Agj1177vtu0MdGI2OVVWcXhUYTNtTDl4SFM2djlESXc",
+                    "0Agj1177vtu0MdElYdnZpb0ctaERXN2VnVmRmR3g3REE"]
 
-SCHEDULER.every '2s' do
-  session = GoogleDrive.login(ENV['USER_NAME'],ENV['PASSWORD'])
-  worksheets = session.spreadsheet_by_key(SPREADSHEET_KEY).worksheets
-  radiators = read_data_as_array_from_doc(worksheets)
+SCHEDULER.every '5s' do
+  radiators = RadiatorReader.new().read_data
   radiators.keys.each do | project_name|
-    send_event(project_name.downcase, radiators[project_name])
-    #send_event(item[:project_name].downcase, { org_radiator_items: item[:org_radiator_items]})
+    send_event(project_name.downcase, {items: radiators[project_name]})
   end
 end
 
-def read_data_as_array_from_doc(worksheets)
-  item_names = ["org_radiator_items","tech_radiator_items","personal_radiator_items"]
-  project_radiator_data = {}
-  for item in 0..2
-    doc = worksheets[item]
-    for row in 2..doc.num_rows
-      data = []
-      project_name = doc[row,1] 
-      for col in 2..doc.num_cols
-        data << {label: doc[1,col],class: "label-#{doc[row,col].downcase}"}  
-      end
+class RadiatorReader
+  @@session = GoogleDrive.login(ENV['USER_NAME'],ENV['PASSWORD'])
+  def initialize
+    @project_radiator_data = {}
+  end
 
-      if(project_radiator_data[project_name])
-        project_radiator_data[project_name].merge!({item_names[item] => data})
-      else
-        project_radiator_data[project_name] = {item_names[item] => data}
-      end
+  def read_data
+    for item in 0..SPREADSHEET_KEYS.size - 1
+      doc = @@session.spreadsheet_by_key(SPREADSHEET_KEYS[item]).worksheets[0]
+      read_each_project_data_as_row(doc)
+    end
+    @project_radiator_data
+  end
+
+  private
+  def read_each_project_data_as_row(doc)
+    for row in 2..doc.num_rows
+      append_radiator_data_for_given_project_row(doc,row)
     end
   end
-  project_radiator_data
+
+  def append_radiator_data_for_given_project_row(doc,row)
+    project_name = doc[row,1] 
+    data = fetch_data_if_exists_for_project_to_append_more_data(project_name)
+    for col in 2..doc.num_cols
+      data << {label: doc[1,col],class: "label-#{doc[row,col].downcase}"}  
+    end
+  end
+
+  def fetch_data_if_exists_for_project_to_append_more_data(project_name)
+    data = @project_radiator_data[project_name]
+    if data.nil?
+      data = [] if data.nil?
+      @project_radiator_data[project_name] = data
+    end
+    data
+  end
 end
