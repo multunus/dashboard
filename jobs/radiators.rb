cd83a451d1cf28ba39ec299ac1ba84df2e32e621
leena <leena.sn@multunus.com>
2013-05-08 17:02:48 +0530

    %1

--- jobs/radiators.rb
+++ jobs/radiators.rb
@@ -1,6 +1,6 @@
 require 'google_drive'
 
-SCHEDULER.every '5s' do
+SCHEDULER.every '30s' do
   radiators = RadiatorReader.new().read_data
   radiators.keys.each do | project_name|
     send_event(project_name.downcase, {items: radiators[project_name]})
