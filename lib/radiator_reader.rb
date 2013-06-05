232f77a09273ab56a3fea2238c9ca030a8ad6aca
Sandeep Venugopal <sandeep.mkv@gmail.com>
2013-06-06 01:40:29 +0530

    fixed bug related to radiator update status

--- lib/radiator_reader.rb
+++ lib/radiator_reader.rb
@@ -75,7 +75,7 @@ class RadiatorReader
   def get_updated_status(updated_date, update_frequency_in_days)
     formated_date = Date.strptime(updated_date, "%m/%d/%Y").strftime("%b-%d")
     todays_date = Date.today.to_s
-    if(Date.today.wday == 2) 
+    if(Date.today.wday == 1) 
       modified_todays_date = DateTime.now.to_date - 2
       (DateTime.parse(modified_todays_date.to_s) - DateTime.parse(formated_date)).to_i > update_frequency_in_days ? "show" : "hide" 
     else
